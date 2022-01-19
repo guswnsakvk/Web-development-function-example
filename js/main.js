a6420f9cbcb714e8938d6d525d647be26b896f93
guswnsakvk <guswnsakvk1@naver.com>
2022-01-19 15:45:41 +0900

    영수증 발급

--- js/main.js
+++ js/main.js
@@ -145,10 +145,10 @@ function ondrop_handler(event){
     const shoppingInfo = document.createElement("div")
     shoppingInfo.classList.add("shoppingInfo")
     const productName = document.createElement("h6")
-    productName.classList.add("product-name")
+    productName.classList.add(`product-name${data}`)
     productName.innerHTML = `${dropTitle.innerText}`
     const productBrand = document.createElement("h6")
-    productBrand.classList.add("product-brand")
+    productBrand.classList.add(`product-brand${data}`)
     productBrand.innerText = `${dropBrand.innerText}`
     const productPrice = document.createElement("h6")
     productPrice.classList.add(`product-price${data}`)
@@ -172,20 +172,17 @@ function ondrop_handler(event){
     shoppingInfo.append(productCount)
 
     const productTotal = document.createElement("h6")
-    productTotal.classList.add("product-total")
+    productTotal.classList.add(`product-total${data}`)
     productTotal.innerText = `합계: ${dropPrice.innerText}`
     shoppingInfo.append(productTotal)
     shoppingCard.append(shoppingInfo)
     shoppingBoxList.append(shoppingCard)
 
     const valueChange = document.querySelector(`.get-count${data}`)
-    console.log(valueChange)
     valueChange.addEventListener("change", ()=>{
       productTotal.innerText = `합계: ${valueChange.value * parseInt(dropPrice.innerText)}`
-      console.log(valueChange.value)
       prices[data] = parseInt(`${valueChange.value * parseInt(dropPrice.innerText)}`)
       PurchasePrice(prices)
-      console.log(prices)
     })
 
     const icon = document.createElement("i")
@@ -209,11 +206,6 @@ function ondrop_handler(event){
     prices[data] = parseInt(`${dropPrice.innerText}`)
     PurchasePrice(prices)
   }
-  // console.log(data)
-  // console.log(drop)
-  // console.log(dropTitle.innerText)
-  // console.log(dropBrand.innerText)
-  // console.log(dropPrice.innerText)
 }
 
 function PurchasePrice(prices){
@@ -223,3 +215,52 @@ function PurchasePrice(prices){
   })
   total.innerText = `총합: ${sum}`
 }
+
+// 구매하기
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext('2d')
+const receipt = document.querySelector(".receipt")
+const today = new Date()
+const closeReceipt = document.querySelector(".close")
+const buyButton = document.querySelector(".buy-button")
+canvas.width = 0
+canvas.height = 0
+
+buyButton.addEventListener("click", function(event){
+  console.log(receipt)
+  receipt.classList.remove("hidden")
+  const year = today.getFullYear()
+  const month = today.getMonth() + 1
+  const date = today.getDate()
+  const hours = today.getHours()
+  const minutes = today.getMinutes()
+  const seconds = today.getSeconds()
+  canvas.width = 500
+  canvas.height = 200 + (120 * dropItemId.length)
+
+  c.font = '24px dotum'
+  c.fillText('영수증', 30, 45)
+
+  c.font = '16px dotum'
+  c.fillText(`${year}-${month}-${date} ${hours}:${minutes}:${seconds}`, 30, 75)
+
+  for(let i=0;i<dropItemId.length;i++){
+    const productName = document.querySelector(`.product-name${dropItemId[i]}`).innerText
+    const productBrand = document.querySelector(`.product-brand${dropItemId[i]}`).innerText
+    const productPrice = document.querySelector(`.product-price${dropItemId[i]}`).innerText
+    const productCount = document.querySelector(`.get-count${dropItemId[i]}`).value
+    const productTotal = document.querySelector(`.product-total${dropItemId[i]}`).innerText
+    
+    c.fillText(productName, 30, 120 * (i+1))
+    c.fillText(productBrand, 30, 120 * (i+1) + 20)
+    c.fillText(`가격 : ${productPrice}`, 30, 120 * (i+1) + 40)
+    c.fillText(`수량 : ${productCount}`, 30, 120 * (i+1) + 60)
+    c.fillText(`${productTotal}`, 30, 120 * (i+1) + 80)
+  }
+
+  c.fillText(`${total.innerText}`, 30, 120 * dropItemId.length + 150)
+})
+
+closeReceipt.addEventListener("click", function(){
+  receipt.classList.add("hidden")
+})
